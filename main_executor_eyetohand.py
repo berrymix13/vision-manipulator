45b20f24f94f802ce3110476280339845c95ca41
Sujin Jeong <sujini7773@gmail.com>
2025-08-28 10:44:43 +0900

    add illustrative image to README

--- main_executor_eyetohand.py
+++ main_executor_eyetohand.py
@@ -1,21 +1,21 @@
 # 전체 실행 흐름 제어
 # nodes/executor.py
-import numpy as np
+import cv2
 import time
+import numpy as np
 from pymycobot import MyCobot280
 from utils.command_parser import parse_command
 from utils.pixel_to_cam_coords import detect_objects
 from utils.camera import capture_d455_images, load_intrinsics
-from utils.cam2base_eyetohand import transform_xyz_cam2base, transform_rpy_cam2base
-from utils.pointcloud_utils import create_roi_pcd_from_depth, calculate_rpy
-from utils.robot_action import pick_and_place
+from utils.cam2base_eyetohand import cam2base
+from utils.grasp_orientation import get_roi_depth_intrinsic, create_pcd_from_depth, segment_plane_from_pointcloud, pcd_to_surface_normal
+from utils.robot_action import move_to_object
 
-PORT = "/dev/ttyACM2"
+
+PORT = "/dev/ttyACM0"
 BAUD = 115200
 GRIPPER_LENGTH_MM = 160
-model_path = "/home/ros/llm_robot/yolo/runs/pose/yolo11n_640_500ep/weights/best.pt"
-xyz_cam2base_path = "/home/ros/llm_robot/data/Calibration/Eye-to-Hand11/cam2base_table_normal_fix_v3.json"
-rpy_cam2base_path = "/home/ros/llm_robot/data/Calibration/Eye-to-Hand11/cam2base_table_normal_fix_v3.json"
+ee2cam_path = "/home/ros/llm_robot/data/Calibration/ee2cam_opt3.json"
 
 def run():
     # 명령 입력 받기
@@ -38,33 +38,36 @@ def run():
     time.sleep(1)
     print("2. 캡쳐 수행")
     c_path, d_path, intr_path = capture_d455_images()
+    coords = mc.get_coords()
     print("3. 캡쳐 완료")
+    print("Saved:", c_path, d_path)
 
     # 카메라 메트릭스 추출
     camera_matrix, dist_coeffs = load_intrinsics(intr_path)
 
     # RGB에서 YOLO로 카메라상 좌표 얻기
-    print("목표 6-DoF 계산 시작")
-    target_list = ["cube"]
+    best_model = "/home/ros/llm_robot/yolo11_seg_cube_best.pt"
+    target_list = [t["name"] for t in cmd["targets"]]
     yolo_outputs = detect_objects(c_path, d_path,target_list, 
-                                  camera_matrix, dist_coeffs, model_path)
-    # Base상 6-DoF    
+                                  camera_matrix, dist_coeffs, best_model)
+    
     cam_xyz = yolo_outputs[0]["cam_xyz"]
     
-    # PCA로 RPY 계산 
-    pcd = create_roi_pcd_from_depth(d_path, yolo_outputs, camera_matrix)
-    roll_normal, pitch_normal, yaw_pca = calculate_rpy(pcd)
-    print(f"[Cam] roll_normal: {roll_normal}, pitch_normal: {pitch_normal}, yaw_pca: {yaw_pca}")
-
-    # 선택된 좌표를 hand-eye calibration결과를 활용해 변환
-    base_xyz = transform_xyz_cam2base(xyz_cam2base_path, cam_xyz)
-    base_rpy = transform_rpy_cam2base([roll_normal, pitch_normal, yaw_pca], rpy_cam2base_path)
-    coords = np.round(base_xyz , 1).tolist() + np.round(base_rpy, 1).tolist()
-    print(f"[Base] coords: {coords}")
-    # 현재 로봇 환경에 맞는 Hard Coding 추가 후 이동
-    print("로봇 이동 시작")
-    pick_and_place(mc, coords)
+    raw_depth = np.load(d_path)
+    roi_depth, roi_intrinsic = get_roi_depth_intrinsic(raw_depth, yolo_outputs, intr_path)
+    pcd = create_pcd_from_depth(roi_depth, roi_intrinsic)
+    pcd = segment_plane_from_pointcloud(pcd)
+    rotation_matrix, yaw_angle = pcd_to_surface_normal(pcd)
     
+    # 선택된 좌표를 hand-eye calibration결과를 활용해 변환
+    base_coords = cam2base(cam_xyz, coords, rotation_matrix, yaw_angle, 
+                        ee2cam_path, ee2cam = False)
+
+    # 현재 로봇 환경에 맞는 Hard Coding 추가 후 이동
+    # move_to_object(mc, base_xyz_mm, gripper_length_mm=GRIPPER_LENGTH_MM)
+    
+
+
 
 if __name__ == "__main__":
     run()
