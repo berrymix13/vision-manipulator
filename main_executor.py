05640e783830f3d053cbb6a2a2bfdd3ad9a35461
Sujin Jeong <sujini7773@gmail.com>
2025-08-29 16:34:38 +0900

    switch cam2base to PGO_inlier result

--- main_executor.py
+++ main_executor.py
@@ -14,26 +14,25 @@ PORT = "/dev/ttyACM2"
 BAUD = 115200
 GRIPPER_LENGTH_MM = 160
 model_path = "/home/ros/llm_robot/yolo/runs/pose/yolo11n_640_500ep/weights/best.pt"
-xyz_cam2base_path = "/home/ros/llm_robot/data/Calibration/Eye-to-Hand11/cam2base_table_normal_fix_v3.json"
-rpy_cam2base_path = "/home/ros/llm_robot/data/Calibration/Eye-to-Hand11/cam2base_table_normal_fix_v3.json"
+cam2base_path = "/home/ros/llm_robot/data/Calibration/Eye-to-Hand11/cam2base_pgo_inliers.json"
 
 def run():
     # 명령 입력 받기
-    user_input = input("명령어 입력: ")
+    # user_input = input("명령어 입력: ")
     
-    # 자연어 명령을 JSON으로 변경
-    cmd = parse_command(user_input)
-    if not cmd:
-        print("⚠️ 명령 분석 실패")
-        return
-    print("✅ LLM 명령 파싱 결과:", cmd)
+    # # 자연어 명령을 JSON으로 변경
+    # cmd = parse_command(user_input)
+    # if not cmd:
+    #     print("⚠️ 명령 분석 실패")
+    #     return
+    # print("✅ LLM 명령 파싱 결과:", cmd)
 
     # 객체 탐지를 위한 이미지 캡쳐 및 뎁스 저장
     print("Intel Real Sense 캡쳐 수행")
     # 로봇 연결 
-    print("1. 로봇 연결 시작")
-    mc = MyCobot280(PORT, BAUD)
-    mc.send_angles([0, 0, 0, -75, 0, 0], 50)
+    # print("1. 로봇 연결 시작")
+    # mc = MyCobot280(PORT, BAUD)
+    # mc.send_angles([0, 0, 0, -75, 0, 0], 50)
     print("...안정화 진행...")
     time.sleep(1)
     print("2. 캡쳐 수행")
@@ -57,13 +56,13 @@ def run():
     print(f"[Cam] roll_normal: {roll_normal}, pitch_normal: {pitch_normal}, yaw_pca: {yaw_pca}")
 
     # 선택된 좌표를 hand-eye calibration결과를 활용해 변환
-    base_xyz = transform_xyz_cam2base(xyz_cam2base_path, cam_xyz)
-    base_rpy = transform_rpy_cam2base([roll_normal, pitch_normal, yaw_pca], rpy_cam2base_path)
+    base_xyz = transform_xyz_cam2base(cam2base_path, cam_xyz)
+    base_rpy = transform_rpy_cam2base([roll_normal, pitch_normal, yaw_pca], cam2base_path)
     coords = np.round(base_xyz , 1).tolist() + np.round(base_rpy, 1).tolist()
     print(f"[Base] coords: {coords}")
     # 현재 로봇 환경에 맞는 Hard Coding 추가 후 이동
     print("로봇 이동 시작")
-    pick_and_place(mc, coords)
+    # pick_and_place(mc, coords)
     
 
 if __name__ == "__main__":
